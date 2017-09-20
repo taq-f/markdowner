54c55ea3913e119e5af7832317fbe7a512d60e39
taq-f <mysecondproject@gmail.com>
2017-09-20 13:38:53 +0900

    add image file copy feature

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"mime"
@@ -40,17 +41,24 @@ func (r *Renderer) Render(path string) error {
 
 	markdowned := blackfriday.MarkdownCommon(data)
 
+	outPath, err := outPath(path, r.OutDir, r.BaseDir)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(filepath.Dir(outPath), os.ModeDir)
+	if err != nil {
+		return err
+	}
+
 	// we need document reader to modify markdowned html text, for example,
 	// syntax highlight.
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(markdowned))
 	if err != nil {
 		return err
 	}
-
-	highlightCode(doc)
-	if r.ImageInline {
-		packImage(doc, r.BaseDir)
-	}
+	r.highlightCode(doc)
+	r.handleImage(doc, filepath.Dir(path))
 
 	content, _ := doc.Html()
 	content = strings.Replace(content, "<html><head></head><body>", "", 1)
@@ -60,18 +68,7 @@ func (r *Renderer) Render(path string) error {
 	output = strings.Replace(output, "{{{style}}}", r.Style, -1)
 	output = strings.Replace(output, "{{{content}}}", content, -1)
 
-	out, err := outPath(path, r.OutDir, r.BaseDir)
-
-	if err != nil {
-		return err
-	}
-
-	err = os.MkdirAll(filepath.Dir(out), os.ModeDir)
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(out, []byte(output), os.ModeAppend)
+	err = ioutil.WriteFile(outPath, []byte(output), os.ModeAppend)
 	if err != nil {
 		return err
 	}
@@ -80,29 +77,56 @@ func (r *Renderer) Render(path string) error {
 }
 
 // highlight inside of code tag
-func highlightCode(doc *goquery.Document) {
+func (r *Renderer) highlightCode(doc *goquery.Document) {
 	doc.Find("code[class*=\"language-\"]").Each(func(i int, s *goquery.Selection) {
 		oldCode := s.Text()
 		formatted, err := syntaxhighlight.AsHTML([]byte(oldCode))
 		if err != nil {
 			log.Fatal(err)
+			return
 		}
 		s.SetHtml(string(formatted))
 	})
 }
 
 // include image to html document
-func packImage(doc *goquery.Document, baseDir string) {
-	doc.Find("img").Each(func(i int, s *goquery.Selection) {
-		src, _ := s.Attr("src")
-		if !strings.HasPrefix(src, "http") {
-			path := filepath.Join(baseDir, src)
+func (r *Renderer) handleImage(doc *goquery.Document, dirPath string) {
+	if r.ImageInline {
+		// include image into html document
+		doc.Find("img").Each(func(i int, s *goquery.Selection) {
+			src, _ := s.Attr("src")
+			if strings.HasPrefix(src, "http") {
+				return
+			}
+
+			path := filepath.Join(dirPath, src)
 			mime := mime.TypeByExtension(filepath.Ext(path))
 			base64, _ := imageToBase64(path)
 			srcEnced := fmt.Sprintf("data:%s;base64,%s", mime, base64)
 			s.SetAttr("src", srcEnced)
-		}
-	})
+		})
+	} else {
+		// move image files to out directory
+		doc.Find("img").Each(func(i int, s *goquery.Selection) {
+			src, _ := s.Attr("src")
+			if strings.HasPrefix(src, "http") {
+				return
+			}
+
+			fromPath := filepath.Join(dirPath, src)
+			toPath := filepath.Join(r.OutDir, dirPath[utf8.RuneCountInString(r.BaseDir):], src)
+			err := os.MkdirAll(filepath.Dir(toPath), os.ModeDir)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			err = copyFile(fromPath, toPath)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+		})
+	}
 }
 
 // get output file name
@@ -137,4 +161,30 @@ func imageToBase64(path string) (string, error) {
 
 	imgEnc := base64.StdEncoding.EncodeToString(image)
 	return imgEnc, nil
+}
+
+// copy file
+func copyFile(srcPath string, destPath string) error {
+	if srcPath == destPath {
+		return nil
+	}
+
+	src, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(destPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
